2f0aca980faba2ecd910de8a272e8c3821237003
zionmezba <mezbaul15-3496@diu.edu.bd>
2024-03-30 17:47:21 +0600

    init

---
+++ .dart_tool/flutter_build/5a3494139b5bc70aece718fa7ac6786c/native_assets.d
@@ -0,0 +1,1 @@
+ F:\\Flutter\\todo_app\\.dart_tool\\flutter_build\\5a3494139b5bc70aece718fa7ac6786c\\native_assets.yaml: 
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\Flutter\\todo_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=F:\\Flutter\\todo_app"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\Flutter\\todo_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\Flutter\\todo_app"
+  "FLUTTER_TARGET=F:\\Flutter\\todo_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8wNDgxN2M5OWM5ZmQ0OTU2ZjI3NTA1MjA0ZjdlMzQ0MzM1ODEwYWVkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\Flutter\\todo_app\\.dart_tool\\package_config.json"
+)
